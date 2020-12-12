fa70606cd13d3178b32470d558831eabae20b2e5
dph <919479850@qq.com>
2020-12-12 14:48:58 +0800

    + sockets

--- netutils/include/netutils.h
+++ netutils/include/netutils.h
@@ -8,4 +8,9 @@ public:
     
     static int create_socket();
     static void make_socket_unblock(int fd);
+    static void bind_and_listen(int fd, const char* ip_addr, uint16_t port);
+    static void handle_error(const char* msg);
+
+
+    static int const MAX_REQUEST = 100;
 };
--- netutils/src/netutils.cpp
+++ netutils/src/netutils.cpp
@@ -1,6 +1,10 @@
 #include "sys/types.h"
 #include "sys/socket.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
 #include "unistd.h"
+#include "stdio.h"
+#include "stdlib.h"
 #include "fcntl.h"
 
 #include "netutils.h"
@@ -18,4 +22,31 @@ NetUtils::make_socket_unblock(int fd)
 {
     int flags = fcntl(fd, F_GETFL, 0);
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+void
+NetUtils::bind_and_listen(int fd, const char* ip_addr, uint16_t port)
+{
+    sockaddr_in in;
+    in.sin_family = AF_INET;
+    in.sin_addr.s_addr = inet_addr(ip_addr);
+    in.sin_port = htons(port);
+
+    if(bind(fd, (struct sockaddr*)&in,
+         sizeof(struct sockaddr)) == -1)
+    {
+        handle_error("bind");
+    }
+    
+    if(listen(fd, MAX_REQUEST) == -1)
+    {
+        handle_error("listen");
+    }
+}
+
+void
+NetUtils::handle_error(const char* msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
